503938b7e53d5477e1fac65431149e9183c33f84
Ruanschx277 <125902884+Ruanschx277@users.noreply.github.com>
2023-05-28 19:49:35 +0200

    231352

---
+++ index.js
@@ -0,0 +1,46 @@
+addCheckbox =>{
+  let first =document.getElementBuId("selected first").value
+  let last = document.getElementById("selected last").value
+  let form = document.getElementById("form").value
+  let Email = document.getElementById("Email").value
+  let Name = document.getElementById("Name").value
+  let Contact = document.getElementById("Contact Number").value
+}
+
+
+
+alert("Student: " +first + " " +last + "has been added to"+ form)
+
+if(selected >= Checkbox){
+  alert("Student: " + first + " " + last + selected)
+  alert("Welcome!Thank you for your interest in the Subject!We will contact via email or alternatively on your phone number")
+}
+
+
+alert("Student: " +first + " " +last + "has not been added to"+ form)
+
+if(selected >= Checkbox){
+  alert("Student: " + first + " " + last + selected)
+  alert("Well,well,well...why don't you want to sign up for our newsletter...you don't seem =to be that interested Name & Surname ! If you want to sign up,you better CHECK that newsletter box!")
+}
+
+
+let Email = document.getElementsByName("Form sheet")
+if(form [i].checked){
+  Value = Options.value
+}
+
+let Contact = document.getElementsByName("Form sheet")
+if(form [i].checked){
+  Value = Options.value
+}
+
+console.log(value);
+
+
+
+
+
+
+
+
